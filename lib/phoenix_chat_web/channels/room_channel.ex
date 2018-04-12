a6d40e390f7395b642fc3662e4d285fa639603fe
Doug Goldie <dgoldie15@gmail.com>
2018-04-12 00:38:24 -0700

    chat with presence

---
+++ lib/phoenix_chat_web/channels/room_channel.ex
@@ -0,0 +1,27 @@
+defmodule PhoenixChat.RoomChannel do
+  use PhoenixChatWeb, :channel
+  alias PhoenixChatWeb.Presence
+
+  def join("room:lobby", _params, socket) do
+    send(self(), :after_join)
+    {:ok, socket}
+  end
+
+  def handle_info(:after_join, socket) do
+    IO.puts "handle_info"
+    push socket, "presence_state", Presence.list(socket)
+    {:ok, _} = Presence.track(socket, socket.assigns.user, %{
+      online_at: inspect(System.system_time(:seconds))
+    })
+    {:noreply, socket}
+  end
+
+  def handle_in("message:new", message, socket) do
+    broadcast! socket, "message:new", %{
+      user: socket.assigns.user,
+      body: message,
+      timestamp: :os.system_time(:milli_seconds)
+    }
+    {:noreply, socket}
+  end
+end
